57f8556b222b6caccbbe141def06cc28124dd33f
Loenna59 <87862694+Loenna59@users.noreply.github.com>
2024-12-07 23:17:20 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.cpp
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.cpp
@@ -4,6 +4,9 @@
 using namespace std;
 
 string solution(int num) {
-    string answer = num % 2 == 0? "Even" : "Odd";
+    // string answer = num % 2 == 0? "Even" : "Odd";
+    
+    // 비트 연산으로 하는 방법이 더 좋다
+    string answer = num & 1? "Odd" : "Even";
     return answer;
 }
