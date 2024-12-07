1ff93a9f12bb99a99ba44442779751b1b59ae7c8
Loenna59 <87862694+Loenna59@users.noreply.github.com>
2024-12-07 18:26:50 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int input;
+
+    std::cin >> input;
+
+    for (int i = 1; i <= input; i++)
+    {
+        std::cout << i << "\n";
+    }
+    
+    return 0;
+}
