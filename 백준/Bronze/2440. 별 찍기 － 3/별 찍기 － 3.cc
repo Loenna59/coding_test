1d4d300ba94b54f665d79af48b3ab50455f98f2f
Loenna59 <87862694+Loenna59@users.noreply.github.com>
2024-12-07 22:17:36 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string star = "*";
+    int num;
+
+    cin >> num;
+
+    for (int j = 0; j < num; j++)
+    {
+        for (int i = 0; i < num - j; i++)
+        {
+            cout << star;
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
