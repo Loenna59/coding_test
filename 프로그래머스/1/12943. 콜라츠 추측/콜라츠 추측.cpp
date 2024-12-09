808cadd74e9923d494cb0f0448e13d2ed0e42643
Loenna59 <87862694+Loenna59@users.noreply.github.com>
2024-12-09 22:38:15 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num) {
+    int answer = -1;
+
+    long long result = (long long)num;
+    
+    for (int i = 0; i < 500; i++)
+    {
+        if (result == 1)
+        {
+            answer = i;
+            break;
+        }
+
+        result = result & 1? result * 3 + 1 : result / 2;
+    }
+    
+    return answer;
+}
