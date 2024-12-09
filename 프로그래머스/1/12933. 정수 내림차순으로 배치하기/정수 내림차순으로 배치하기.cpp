c487b63afc32aa9567569067a38000400ba6d07f
Loenna59 <87862694+Loenna59@users.noreply.github.com>
2024-12-09 20:22:01 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
@@ -10,20 +10,26 @@ long long solution(long long n) {
     
     string str = to_string(n);
 
-    vector<int> vec;
+// 내가 작성한 것
+//     vector<int> vec;
 
-    for (int i = 0; i < str.size(); i++)
-    {
-        vec.push_back(str[i] ^ 48);
-    }
+//     for (int i = 0; i < str.size(); i++)
+//     {
+//         vec.push_back(str[i] ^ 48);
+//     }
 
-    sort(vec.begin(), vec.end());
+//     sort(vec.begin(), vec.end());
 
-    for (int i = 0; i < vec.size(); i++)
-    {
-        long long val = vec[i] * pow(10, i);
-        answer += val;
-    }
+//     for (int i = 0; i < vec.size(); i++)
+//     {
+//         long long val = vec[i] * pow(10, i);
+//         answer += val;
+//     }
+
+// string으로 sort하면 되네;;
+    sort(str.rbegin(), str.rend());
+
+    answer = stoll(str);
     
     return answer;
 }
