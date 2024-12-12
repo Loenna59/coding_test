9443936b95480b0d02dbd7beaf482faf35682a75
Loenna59 <87862694+Loenna59@users.noreply.github.com>
2024-12-12 22:36:46 +0900

    [level 1] Title: 직사각형 별찍기, Time: 3.45 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    cin >> a >> b;
+    
+    string stars;
+
+    stars.assign(a, '*');
+
+    for (int i = 0; i < b; i++)
+    {
+        cout << stars << endl;
+    }
+    
+    return 0;
+}
