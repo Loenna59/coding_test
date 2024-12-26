c4f1639068222083b344986be06c1b2b507c9e67
Loenna59 <87862694+Loenna59@users.noreply.github.com>
2024-12-27 08:58:14 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (j < n - i - 1)
+            {
+                cout << " ";
+                continue;
+            }
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (j < i)
+            {
+                cout << " ";
+                continue;
+            }
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
