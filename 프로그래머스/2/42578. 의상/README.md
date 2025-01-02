ee0b5efdb962b544644acd50b64c5a3f425ca9df
Loenna59 <87862694+Loenna59@users.noreply.github.com>
2025-01-03 08:07:57 +0900

    [level 2] Title: 의상, Time: 0.05 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
 
 ### 성능 요약
 
-메모리: 4.27 MB, 시간: 0.03 ms
+메모리: 4.22 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 03일 08:04:03
+2025년 01월 03일 08:07:55
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -4,6 +4,9 @@
 
 using namespace std;
 
+// 각 종류의 의상을 1개를 입거나 안 입을 경우의 수를 계산하고 모두 곱한다.
+// 최소 1가지의 의상을 입어야하므로 아무거도 안 입는 1가지는 제외해야한다.
+
 int solution(vector<vector<string>> clothes) {
     int answer = 0;
     
@@ -20,7 +23,7 @@ int solution(vector<vector<string>> clothes) {
         categories[clothes[i][1]]++;
     }
 
-    for(pair<string, int> p : categories)
+    for (pair<string, int> p : categories)
     {
         if (answer == 0)
         {
