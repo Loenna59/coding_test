ffecaff74f6d4d32882e83a42d39d25e206f9932
Loenna59 <87862694+Loenna59@users.noreply.github.com>
2025-01-08 00:38:38 +0900

    [level 2] Title: 프로세스, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> priorities, int location) {
+    int answer = 0;
+
+    queue<pair<int, int>> queue;
+
+    for (int i = 0; i < priorities.size(); i++)
+    {
+        queue.push({i, priorities[i]});
+    }
+
+    sort(priorities.rbegin(), priorities.rend());
+
+    while (!queue.empty())
+    {
+        pair<char, int> proccess = queue.front();
+        queue.pop();
+
+        if (proccess.second == priorities[answer])
+        {
+            if (proccess.first == location)
+            {
+                return answer + 1;
+            }
+            
+            answer++;
+            continue;
+        }
+
+        queue.push(proccess);
+    }
+
+    return -1;
+}
