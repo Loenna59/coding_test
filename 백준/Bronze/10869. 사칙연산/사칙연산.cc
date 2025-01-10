8f06bc79679a6b40136574e8d03bc3693300662c
Loenna59 <87862694+Loenna59@users.noreply.github.com>
2025-01-10 14:52:25 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int a;
+    int b;
+
+    std::cin >> a >> b;
+    std::cout << a + b << "\n";
+    std::cout << a - b << "\n";
+    std::cout << a * b << "\n";
+    std::cout << a / b << "\n";
+    std::cout << a % b;
+}
