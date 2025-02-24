0dab544d3547aa87c0100d6483b0758a76323fb1
Loenna59 <87862694+Loenna59@users.noreply.github.com>
2025-02-25 07:05:59 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int n; // 동전의 종류 개수
+    int k; // 만들자하는 가치
+
+    std::cin >> n >> k;
+
+    std::vector<long> values(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> values[i];
+    }
+
+    int count = k / values.back();
+    int remain = k % values.back();
+    
+    if (remain == 0)
+    {
+        std::cout << count;
+
+        return 0;
+    }
+
+    int index = values.size() - 1;
+
+    while (index >= 0)
+    {
+        index--;
+
+        count += remain / values[index];
+        remain = remain % values[index];
+
+        if (remain == 0)
+        {
+            break;
+        }
+    }
+
+    std::cout << count;
+
+    return 0;
+}
