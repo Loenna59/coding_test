2de5d2b452d9fac1e57b22e0fdfa74b6886921d2
Loenna59 <87862694+Loenna59@users.noreply.github.com>
2025-03-14 00:36:58 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{    
+    const int opening_year = 1946;
+
+    long long n;
+    
+    std::cin >> n;
+    
+    std::cout << n - opening_year;
+
+    return 0;
+}
