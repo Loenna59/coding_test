f99648ef1de1f2f6be0c2d71c269f65b66ba50c9
Loenna59 <87862694+Loenna59@users.noreply.github.com>
2025-03-19 20:23:10 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main()
+{    
+    std::string name;
+
+    std::cin >> name;
+
+    std::cout << ":fan::fan::fan:\n:fan::";
+    std::cout << name << "::fan:\n:fan::fan::fan:";
+
+    return 0;
+}
