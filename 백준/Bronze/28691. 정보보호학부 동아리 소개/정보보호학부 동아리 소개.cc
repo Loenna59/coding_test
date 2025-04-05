2c73d732f87c61aaf0fe26b898133ca5c3fb5102
Loenna59 <87862694+Loenna59@users.noreply.github.com>
2025-04-05 21:30:30 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main()
+{
+
+    char c;
+    std::cin >> c;
+
+    switch (c)
+    {
+        case 'M':
+            std::cout << "MatKor";
+            break;
+        case 'W':
+            std::cout << "WiCys";
+            break;
+        case 'C':
+            std::cout << "CyKor";
+            break;
+        case 'A':
+            std::cout << "AlKor";
+            break;
+        default:
+            std::cout << "$clear";
+            break;
+    }
+
+    return 0;
+}
